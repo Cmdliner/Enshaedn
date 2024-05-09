bc228fdbb28212e47254f5f6cd0f17171b5aa7f4
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-05-09 17:24:33 +0100

    Socket.io working

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,40 @@
 import { createServer } from "http";;
 import express from "express";
-import { Server } from "socket.io"
+import { Server, Socket } from "socket.io"
 import authRouter from "./routes/auth";
 import roomRouter from "./routes/room";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import authMiddleware from "./middlewares/authMiddleware";
+import cors, { type CorsOptions } from "cors";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 const path = "/api/v1";
+const corsOptions: CorsOptions = {
+    origin: process.env.CORS_ORIGIN!,
+    credentials: true,
+    methods: ['GET', 'POST']
+}
 
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.get('/chat', authMiddleware.requireAuth, chatController.handleChatEvents);
 app.use(`${path}/auth`, authRouter);
 app.use(`${path}/rooms`, authMiddleware.requireAuth, roomRouter);
 app.get('/protected', authMiddleware.requireAuth, (req: any, res: any) => res.status(200).json("Protected by a Shaed!"));
 
-// Chat functionalities not implemented
-io.on('connection', (socket) => {
-    // socket.on('join', ());
-    // socket.on('sendMessage', () => );
-    // socket.on('leave', () => {});
-    // socket.on('deleteMssg', () => {});
-    // socket.emit('Hello, world!')
-});
+io.attach(server, { cors: corsOptions });
+io.on('connection', (socket: Socket) => {
+    console.log('\nNew one ');
+    console.log(`Socket Connected => ${socket.id} \n`);
+})
+
 
 mongoose.connect(process.env.MONGO_URI!)
     .then(() => {
